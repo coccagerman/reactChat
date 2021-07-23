2020b5435762ef2bd8d0bd9ff6d9fd454154f1ab
Germán <coccagerman@gmail.com>
2021-07-23 11:06:10 -0300

    .

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -13,11 +13,16 @@ export  default function Login() {
         auth.signInWithPopup(provider)
     }
 
+    const signInWithFacebook = () => {
+        const provider = new firebase.auth.FacebookAuthProvider()
+        auth.signInWithPopup(provider)
+    }
+
     return (
         <section className='login'>
             <p>Please sign in</p>
             <button onClick={() => signInWithGoogle()}><Icon icon={googleFill} className='icon'/>Sign in with Google</button>
-            <button onClick={() => console.log('signInWithFacebook')}><Icon icon={facebookFill} className='icon'/>Sign in with Facebook</button>
+            <button onClick={() => signInWithFacebook()}><Icon icon={facebookFill} className='icon'/>Sign in with Facebook</button>
             <button onClick={() => console.log('signInWithTwitter')}><Icon icon={twitterFill} className='icon'/>Sign in with Twitter</button>
             <button onClick={() => console.log('signInWithGithub')}><Icon icon={githubFill} className='icon'/>Sign in with Github</button>
         </section>
